58787728b5bf9db8036f91d26e594f13384d1dc3
Jeroen Dunselman <jeroduns@gmail.com>
2020-03-03 10:35:55 +0100

    Initial Commit

---
+++ Coded Layout LabTests/Coded_Layout_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  Coded_Layout_LabTests.swift
+//  Coded Layout LabTests
+//
+//  Created by Jeroen Dunselman on 03/03/2020.
+//  Copyright © 2020 Jeroen Dunselman. All rights reserved.
+//
+
+import XCTest
+@testable import Coded_Layout_Lab
+
+class Coded_Layout_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
